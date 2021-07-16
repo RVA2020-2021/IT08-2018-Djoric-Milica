86ae02a5eecdb0ff476b87e1bb650d409e97dd41
DjoricMilic <djoric.it8.2018@uns.ac.rs>
2021-07-17 00:58:34 +0100

    izmena putanje

--- AngularStatrerProject/src/app/app.constants.ts
+++ AngularStatrerProject/src/app/app.constants.ts
@@ -1,5 +1,12 @@
+/*
 export const PROJEKAT_URL = 'http://localhost:8083/projekat';
 export const SMER_URL = 'http://localhost:8083/smer';
 export const GRUPA_URL = 'http://localhost:8083/grupa';
 export const STUDENT_URL = 'http://localhost:8083/student';
 export const STUDENTI_U_GRUPI_URL = 'http://localhost:8083/studentiZaGrupaID';
+*/
+export const PROJEKAT_URL = 'https://backend-rva-it8.herokuapp.com/projekat';
+export const SMER_URL = 'https://backend-rva-it8.herokuapp.com/smer';
+export const GRUPA_URL = 'https://backend-rva-it8.herokuapp.com/grupa';
+export const STUDENT_URL = 'https://backend-rva-it8.herokuapp.com/student';
+export const STUDENTI_U_GRUPI_URL = 'https://backend-rva-it8.herokuapp.com/studentiZaGrupaID';
